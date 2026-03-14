b8fbe005a49d3ff079e65fe96e6cfe551909336a
Naveena <naveenaadvani@gmail.com>
2026-03-14 11:41:39 +0530

    Delete fstask1.js

--- fstask1.js
+++
@@ -1,9 +0,0 @@
-var fs = require("fs");
-// fs.mkdirSync("task")
-fs.writeFileSync("task/a.txt", "hello, Good morning");
-fs.appendFileSync("task/a.txt", "\n new data added");
-// var r = fs.readFileSync("task/a.txt","utf-8");
-// fs.writeFileSync("task/a1.txt", r);
-// fs.unlinkSync("task/a.txt")
-// fs.copyFileSync("task/a.txt","task/a2.txt");
-fs.renameSync("task/a2.txt", "task/new.txt");
